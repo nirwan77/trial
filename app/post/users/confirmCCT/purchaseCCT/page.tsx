e70984fbfd22f0dab11ab66b8a28fb0ed3227b8c
Nirwan Rai <nirwanrai6969@gmail.com>
2024-02-22 11:09:31 +0545

    finished redesigning of publish post page

--- app/post/users/confirmCCT/purchaseCCT/page.tsx
+++ app/post/users/confirmCCT/purchaseCCT/page.tsx
@@ -20,27 +20,25 @@ function App(): JSX.Element {
             alt="back arrow"
           />
         </button>
-        <h2 className="font-black text-sm leading-5">
+        <h2 className="font-[425] text-SoshColorGrey700 leading-5">
           Purchase CCT for your post
         </h2>
       </div>
 
-      <div className="flex flex-col gap-2 items-center justify-center mb-12 p-4">
+      <div className="flex flex-col py-8 gap-4 rounded-2xl min-w-[360px] bg-green-700 items-center justify-center mb-11 p-4">
         <Image
           className="w-auto"
           priority
           src={"/ETH.svg"}
           alt="ETH"
-          width={24}
-          height={24}
+          width={64}
+          height={85}
         />
 
-        <div className="flex gap-2 font-black text-sm leading-5 text-SoshColorGrey600">
-          1 CCT
-        </div>
-        <div className="flex gap-2 font-black text-sm leading-5 text-SoshColorGrey600">
+        <div className="flex gap-2 text-2xl leading-5 text-white">1 CCT</div>
+        <div className="flex gap-2 leading-5 text-white">
           <Image
-            className="w-auto"
+            className="w-auto stroke-white"
             priority
             src={"/transfer.svg"}
             alt="Follow us on Twitter"
@@ -51,40 +49,26 @@ function App(): JSX.Element {
         </div>
       </div>
 
-      <div className="flex w-96 flex-col mb-8 py-4 px-8 gap-4 items-start rounded-2xl bg-SoshColorBento">
-        <div className="w-full flex justify-between">
-          <p className="font-black text-base leading-6 text-SoshColorGrey500">
-            Amount Total
-          </p>
-          <p className="font-black leading-4 text-xs text-SoshColorGrey600">
-            1 CCT
-          </p>
+      <div className="flex min-w-[360px] flex-col mb-8 py-4 px-8 gap-4 items-start rounded-2xl sosh__background border border-SoshColorGrey300">
+        <div className="flex justify-between w-full text-SoshColorGrey600">
+          <div className="text-sm leading-Sosh22 text-black">Amount Total</div>
+          <div className="text-xs leading-Sosh22">1 CCT</div>
         </div>
-        <div className="w-full flex justify-between">
-          <p className="font-black text-base leading-6 text-SoshColorGrey500">
-            Sosh Fee
-          </p>
-          <p className="font-black leading-4 text-xs text-SoshColorGrey600">
-            140 Sosh
-          </p>
+        <div className="flex justify-between w-full text-SoshColorGrey600">
+          <div className="text-sm leading-Sosh22">Tax Fee</div>
+          <div className="text-xs leading-Sosh22">140 SST | $140 </div>
         </div>
-        <div className="w-full flex justify-between">
-          <p className="font-black text-base leading-6 text-SoshColorGrey500">
-            Total
-          </p>
-          <div className="flex flex-col font-black leading-4 text-xs text-SoshColorGrey600">
-            <p>2000 Sosh</p>
-            <p>= $40</p>
-            <p>= 0.23ETH</p>
-          </div>
+        <div className="flex justify-between w-full text-SoshColorGrey600">
+          <div className="text-sm leading-Sosh22 text-black">Total Cost</div>
+          <div className="text-xs leading-Sosh22">2000 SST | $120</div>
         </div>
       </div>
 
       <button
-        className="p-4 w-96 max-w-96 rounded-2xl font-black bg-SoSHColorPrimary text-white"
-        onClick={() => router.push("")}
+        className="p-4 w-96 text-sm max-w-96 rounded-2xl font-[425] leading-Sosh22 bg-green-700 text-white"
+        onClick={() => router.push("/post/users/poststatus")}
       >
-        Publish
+        Purchase
       </button>
     </div>
   );
